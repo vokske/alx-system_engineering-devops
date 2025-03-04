31513cc6be31c19821e4718fc91ab6f02924b631
vokske <kevinmuigai2@gmail.com>
2025-03-04 12:24:34 +0300

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,7 @@
 # Install the 'flask' package from pip3
 # Ensure Python3 and pip3 packages are installed first
 
-package { 'python3':
-    ensure => 'installed',
-}
-
-package { 'python3-pip':
-    ensure => 'installed',
-}
-
-package { 'install_flask':
+package { 'flask':
     ensure   => '2.1.0',
-    name     => 'flask',
     provider => 'pip3',
 }
-
-# Ensure dependencies are installed before the Flask package
-Package['python3'] -> Package['python3-pip'] -> Package['install_flask']
